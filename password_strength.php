5cdcdac87d579f7a54a242c15d1e47a33c303440
romanzc2011 <93228839+romanzc2011@users.noreply.github.com>
2022-07-05 16:51:07 -0500

    Update password_strength.php

--- password_strength.php
+++ password_strength.php
@@ -1,81 +1,44 @@
 <?php
-$pass = isset($_GET['rate']) ? $_GET['rate'] : '0';
-$pass_length = strlen(trim($pass));
-$rating = 0;
-$upper_count = 0;
-$lower_count = 0;
-$num_count = 0;
-$symbol_count = 0;
-$length_pts = 0;
-
-for($i=0; $i < $pass_length; $i++){
-   if(ctype_lower($pass[$i]) === true){
-       $lower_count++;
-       if($lower_count === 1){ $rating += 1; }
-   }
-   if(ctype_upper($pass[$i]) === true){
-       $upper_count++;
-       if($upper_count === 1){ $rating += 1l}
-   }
-   echo 'upper '.$upper_count.'<br>';
-   echo 'lower '.$lower_count.'<br>';
-   echo 'num '.$num_count.'<br>';
-   echo 'sym '.$symbol_count.'<br>';
-
-   if($upper_count >= 1){ $rating ++; }
-   if($lower_count >= 1){ $rating += 1; }
-   if($num_count >= 2){ $rating += 2; }
-   if($num_count > 1){ $rating += 1; }
-   if($symbol_count >= 2){ $rating += 2; }
-   if($symbol_count > 1){ $rating += 1;}
-   if($pass_length === 8){ $rating += 2; }
-   if($pass_length > 8){
-       for($j = 0; $j <= 16; $j++){ $pass_length += 0.5; }
-       $length_pts += 2;
-   }
+function detect_lowercase($string){
+    return strtolower($string) != $string;
 }
 
-$rating += $length_pts;
+function detect_uppercase($string){
+    return strtoupper($string) != $string;
+}
+
+function count_numbers($string){
+    return preg_match_all('/[0-9]/', $string);
+}
+
+function count_symbols($string){
+    return preg_match_all('/[!@#$%^&*]/', $string);
+}
+
+function password_strength($password){
+    $strength = 0;
+    $possible_points = 12;
+    $length = strlen($password);
+
+    if(detect_lowercase($password)){ $strength += 1; }
+    if(detect_uppercase($password)){ $strength += 1; }
+
+    $strength += min(count_numbers($password), 2);
+    $strength += min(count_symbols($password), 2);
+
+    if(strlen($password) >= 8){
+        $strength += 2;
+        $strength += min(($length - 8) * 0.5, 4);
+    }
+
+    $strength_precent = $strength / (float)$possible_points;
+    $rating = floor($strength_precent * 10);
+    return $rating;
+}
+
+if(isset($_POST['rate'])){
+    $password = $_POST['rate'];
+    echo password_strength($password);
+}
 
 ?>
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/4380f7e432.js" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-</head>
-<body>
-<form>
-<div class="container-fluid">
-<p>Your password rating is: <?php echo $rating; ?></p>
-
-<p>Rate the strength of your password:</p>
-Password: <input type="text" name="rate" id="rate" value="">
-<input type="submit" id="submit" name="submit" value="Submit">
-</div>
-<script>
-    $(document).ready(function(){
-        $('#submit').click(function(){
-            let rate = $('#rating').val();
-
-            $.ajax({
-                type: "GET",
-                url: "password_strength.php",
-                data: {
-                    rate: rate
-                }
-            });
-        });
-    });
-</script>
-</form>
-</body>
-</html>
