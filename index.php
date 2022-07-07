07d88c0641b5c97e32984b9b7a7395bf557ecc13
romanzc2011 <93228839+romanzc2011@users.noreply.github.com>
2022-07-07 16:36:35 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -7,27 +7,45 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Password Strength</title>
     <style>
-        #meter div {
+        .box {
             height: 20px; width: 20px;
             margin: 0 1px 0 0; padding: 0;
             float: left;
             background-color: #dddddd;
         }
-        #meter div.rating-1, #meter div.rating-2 {
+        .rating-1,.rating-2 {
+            height: 20px; width: 20px;
+            margin: 0 1px 0 0; padding: 0;
+            float: left;
             background-color: red;
         }
-        #meter div.rating-3, #meter div.rating-4 {
+        .rating-3,.rating-4 {
+            height: 20px; width: 20px;
+            margin: 0 1px 0 0; padding: 0;
+            float: left;
             background-color: orange;
         }
-        #meter div.rating-5, #meter div.rating-6 {
+        .rating-5,.rating-6 {
+            height: 20px; width: 20px;
+            margin: 0 1px 0 0; padding: 0;
+            float: left;
             background-color: yellow;
         }
-        #meter div.rating-7, #meter div.rating-8 {
+        .rating-7,.rating-8 {
+            height: 20px; width: 20px;
+            margin: 0 1px 0 0; padding: 0;
+            float: left;
             background-color: blue;
         }
-        #meter div.rating-9, #meter div.rating-10 {
+        .rating-9,.rating-10 {
+            height: 20px; width: 20px;
+            margin: 0 1px 0 0; padding: 0;
+            float: left;
             background-color: green;
+
         }
+
+
     </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -38,24 +56,24 @@
 <form id="form" action="index.php" method="post">
 <div class="container-fluid">
 <p>Your password rating is: <span id="response"></p>
-
-    <div id="meter">
         <?php
-            for($i=0;$i < 10; $i++){
-                print('<div></div>');
+            for($i = 1; $i <= 10; $i++){
+                echo '<div class="box" id="'.$i.'"></div>';
             }
         ?>
-    </div>
-    <br style="clear: both;">
-<p>Rate the strength of your password:</p>
-Password: <input type="text" name="rate" id="rate" value="">
     <br>
-<input type="submit" id="submit" name="submit" value="Submit">
-
+<p>Strength of your password:</p>
+<span id="container"></span>
+    <br>
+Password: <input type="text" name="rate" id="rate" value="">
+    <br><br>
+    <button class="btn btn-success" id="submit" name="submit">Submit</button>
 </div>
+
 <script>
     $(document).ready(function(){
-       $('#form').submit(function(e){
+
+       $('#submit').click(function(e){
 
            $.ajax({
               type:"POST",
@@ -63,7 +81,13 @@ Password: <input type="text" name="rate" id="rate" value="">
               data:$('form').serialize(),
                success:function(data){
                   $('#response').html(data);
+                  for(let i = 1; i <= data; i++){
+                     $('#'+i).removeClass('box')
+                         .addClass('rating-'+i);
+                  }
+
                }
+
            });
            e.preventDefault();
        });
